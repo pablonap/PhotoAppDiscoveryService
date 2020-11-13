18583ce691d5243d74541701cfa921c914a31a68
chipo <pnapoli33@gmail.com>
2020-11-13 13:00:14 -0300

    Initial commit

---
+++ src/test/java/com/binary_winters/photoapp/discovery/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binary_winters.photoapp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
